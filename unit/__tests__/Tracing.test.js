3c69620f88253a6c9c1f9a08569bc6296193b852
saikrishna321 <saikrishna321@yahoo.com>
2019-03-11 16:36:57 +0000

    added unit test for speedIndex and getTracingLogs

--- unit/__tests__/Tracing.test.js
+++ unit/__tests__/Tracing.test.js
@@ -1,5 +1,5 @@
 import TracingHandler from '../../src/tracingHandler';
-
+import tracingEvents from '../__fixtures__/tracingEvents.json'
 test('Start Tracing', async () => {
   jest.setTimeout(30000);
   const mockedTracingStart = jest.fn();
@@ -8,7 +8,7 @@ test('Start Tracing', async () => {
     client: jest.fn(() => ({ Tracing: { start: mockedTracingStart }, IO: { read: mockedTracingIO } }))
   };
   let tracing = new TracingHandler(mockedTaiko.client().Tracing, mockedTaiko.client().IO);
-  tracing.startTracing();
+  await tracing.startTracing();
   expect(await tracing.isTracing).toBe(true);
   expect(mockedTracingStart).toHaveBeenCalled();
 });
@@ -25,4 +25,27 @@ test('End tracing throws if not tracing', async () => {
   } catch (err) {
     expect(err.message).toEqual('No tracing was initiated, initiate `startTracing()` first');
   }
+})
+
+test('getTracingLogs', async () => {
+  const mockedTracingStart = jest.fn();
+  const mockedTracingIO = jest.fn();
+  const mockedTaiko = {
+    client: jest.fn(() => ({ Tracing: { start: mockedTracingStart }, IO: { read: mockedTracingIO } }))
+  };
+  let tracing = new TracingHandler(mockedTaiko.client().Tracing, mockedTaiko.client().IO);
+  tracing.traceEvents = 'foobar';
+  expect(await tracing.getTracingLogs()).toEqual('foobar');
+})
+
+test('get speed index', async () => {
+  const mockedTracingStart = jest.fn();
+  const mockedTracingIO = jest.fn();
+  const mockedTaiko = {
+    client: jest.fn(() => ({ Tracing: { start: mockedTracingStart }, IO: { read: mockedTracingIO } }))
+  };
+  let tracing = new TracingHandler(mockedTaiko.client().Tracing, mockedTaiko.client().IO);
+  tracing.traceEvents = tracingEvents;
+  const speedIndex = await tracing.getSpeedIndex();
+  expect(speedIndex).toEqual({ speedIndex: 735.2189999735356,perceptualSpeedIndex: 832.5956931637686 })
 })
