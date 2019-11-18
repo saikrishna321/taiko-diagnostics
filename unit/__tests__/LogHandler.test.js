3b9f137c92d8d8582f12529920241a1b0d38befc
Sai <saikrishna321@yahoo.com>
2019-11-18 06:49:06 +0000

    #149 fix test and return emitter

--- unit/__tests__/LogHandler.test.js
+++ unit/__tests__/LogHandler.test.js
@@ -8,31 +8,27 @@ let entryAdded,
   runTime,
   logHandler,
   exceptionThrown,
-  consoleAPICalled;
+  consoleAPICalled,
+  emitter;
 beforeEach(() => {
   entryAdded = jest.fn();
   loadEventFired = jest.fn();
   exceptionThrown = jest.fn();
   consoleAPICalled = jest.fn();
+  emitter = {
+    emitObject: jest.fn()
+  };
   runTime = {
     exceptionThrown: exceptionThrown,
     consoleAPICalled: consoleAPICalled
   };
   logHandler = new LogHandler(entryAdded, loadEventFired, runTime);
-  logHandler.logEntry();
+  logHandler.logEntry(emitter);
 });
 
 test('Should invoke exceptionThrown callback with a listener function', async () => {
   expect(runTime.exceptionThrown.mock.calls.length).toBe(1);
   expect(typeof runTime.exceptionThrown.mock.calls[0][0]).toBe('function');
-});
-
-test('Should invoke logger with appropriate log message when listener passed to exceptionThrown is invoked', async () => {
-  const exceptionThrownListner = await runTime.exceptionThrown.mock.calls[0][0];
-  exceptionThrownListner({
-    exceptionDetails: { exception: { description: 'error message' } }
-  });
-  expect(logger.mock.calls[0][0]).toBe('error message');
 });
 
 test('Should invoke consoleAPI callback with a listener function', async () => {
@@ -43,16 +39,4 @@ test('Should invoke consoleAPI callback with a listener function', async () => {
 test('Should invoke entryAdded callback with a listner function', async () => {
   expect(entryAdded.mock.calls.length).toBe(1);
   expect(typeof entryAdded.mock.calls[0][0]).toBe('function');
-});
-
-test('Should invoke logger with appropriate log message when listener passed to entryAdded is invoked', async () => {
-  const logEntry = entryAdded.mock.calls[0][0];
-  logEntry({
-    entry: { level: 'error', source: 'network', url: 'http://localhost:8080' }
-  });
-  expect(logger.mock.calls[1][0]).toEqual({
-    level: 'error',
-    source: 'network',
-    url: 'http://localhost:8080'
-  });
 });
